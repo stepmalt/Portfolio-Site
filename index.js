66a2dd65d1423bad5e5f986abfb65c39c40fa2a9
Sean Larabell <smlarabell@gmail.com>
2025-01-02 19:23:39 -0500

    respond to comments

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var isOpen = false;
 
+function closeMobileView() {
+    if (isOpen) {
+        openMobileNav()
+    }
+}
+
 function openMobileNav() {
     mobilenav.classList.remove("closed", "open");
     if (isOpen) {
         mobilenav.classList.add("closed");
+        hamburger.src = "assets/graphics/hamburger.svg"
         isOpen = false;
     } else {
         mobilenav.classList.add("open");
+        hamburger.src = "assets/graphics/x.svg"
         isOpen = true;
     }
 } 
